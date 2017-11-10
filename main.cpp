67716e3d314a467918ab3f976f7c9e1bfaf3967c
ilya41299 <32190925+ilya41299@users.noreply.github.com>
2017-11-10 11:23:06 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <string>
 using namespace std;
 
-void sort (unsigned int *mas, unsigned int n){
+void sort (int *mas, int n){
     for(unsigned int i=0; i<n/2; ++i){
         swap(mas[i], mas[n-i-1]);
     }
@@ -11,10 +11,14 @@ void sort (unsigned int *mas, unsigned int n){
 
 int main() 
 { 
-    unsigned int n; 
-    unsigned int *mas; 
+    int n; 
+    int *mas; 
         if (cin >> n){
-            mas = new unsigned int [n];
+            if (n < 0){
+            cout<<"An error has occurred while reading input data"<<endl; 
+            return -1; 
+            }   
+            mas = new int [n]; 
         } 
     else { 
         cout<<"An error has occurred while reading input data"<<endl; 
@@ -28,8 +32,12 @@ int main()
             if(!(stream >> mas[i])){ 
                 cout<<"An error has occurred while reading input data"<<endl; 
                 delete[]mas; 
-                return -1; 
+                return -1;     
             } 
+            if(!(stream.eof())){
+        cout<<"An error has occured while reading input data."<<endl;
+        return -1;
+    }
         } 
     sort (mas, n); 
         for(unsigned int i=0; i<n; i++){ 
